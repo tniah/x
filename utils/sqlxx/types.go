505e283d199d6bcff041f98096e17d32a1a3785a
Makai <tronghaibk2008@gmail.com>
2025-10-10 17:20:03 +0700

    Update types

--- utils/sqlxx/types.go
+++ utils/sqlxx/types.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/tidwall/gjson"
+	"time"
 )
 
 type StringSliceJSON []string
@@ -106,4 +107,82 @@ func (ns *NullInt64) UnmarshalJSON(data []byte) error {
 
 	ns.Valid = true
 	return errors.WithStack(json.Unmarshal(data, &ns.Int))
+}
+
+type NullTime time.Time
+
+func (nt *NullTime) Scan(value interface{}) error {
+	var v sql.NullTime
+	if err := (&v).Scan(value); err != nil {
+		return err
+	}
+	*nt = NullTime(v.Time)
+	return nil
+}
+
+func (nt NullTime) Value() (driver.Value, error) {
+	return sql.NullTime{Valid: !time.Time(nt).IsZero(), Time: time.Time(nt)}.Value()
+}
+
+func (nt NullTime) MarshalJSON() ([]byte, error) {
+	var t *time.Time
+	if !time.Time(nt).IsZero() {
+		tt := time.Time(nt)
+		t = &tt
+	}
+	return json.Marshal(t)
+}
+
+func (nt *NullTime) UnmarshalJSON(data []byte) error {
+	var t time.Time
+	if err := json.Unmarshal(data, &t); err != nil {
+		return err
+	}
+	*nt = NullTime(t)
+	return nil
+}
+
+type NullBool struct {
+	Bool  bool
+	Valid bool // Valid is true if Bool is not NULL
+}
+
+func (nb *NullBool) Scan(value interface{}) error {
+	var d = sql.NullBool{}
+	if err := d.Scan(value); err != nil {
+		return err
+	}
+
+	nb.Bool = d.Bool
+	nb.Valid = d.Valid
+	return nil
+}
+
+func (nb NullBool) Value() (driver.Value, error) {
+	if !nb.Valid {
+		return nil, nil
+	}
+
+	return nb.Bool, nil
+}
+
+func (nb NullBool) MarshalJSON() ([]byte, error) {
+	if !nb.Valid {
+		return []byte("null"), nil
+	}
+
+	return json.Marshal(nb.Bool)
+}
+
+func (nb *NullBool) UnmarshalJSON(data []byte) error {
+	if nb == nil {
+		return errors.New("json.RawMessage: UnmarshalJSON on nil pointer")
+	}
+
+	if len(data) == 0 || string(data) == "null" {
+		return nil
+	}
+
+	nb.Valid = true
+	return errors.WithStack(json.Unmarshal(data, &nb.Bool))
 }
